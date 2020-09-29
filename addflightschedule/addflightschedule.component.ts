306f54011d16750eca72911cd5a529ab6092c0d9
jotheeswar <46890244+jotheeswar@users.noreply.github.com>
2020-09-29 13:20:01 +0530

    Add files via upload

---
+++ addflightschedule/addflightschedule.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-addflightschedule',
+  templateUrl: './addflightschedule.component.html',
+  styleUrls: ['./addflightschedule.component.css']
+})
+export class AddflightscheduleComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
